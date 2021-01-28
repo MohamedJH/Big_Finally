af5fb16192c64c95592ac54d28593c0ad69d1d71
MohamedJH <mohamedjahha0@gmail.com>
2021-01-28 23:32:30 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,14 @@ app.use("/api/quizzes", quizzesRouter)
 // app.use("/api/auth", authRouter)
 
 // Serve static assets if in production
-/*if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static(path.join(__dirname, '../client/build')));
   
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
     });
-  }*/
+  }
 
 
 
